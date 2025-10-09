a418f8c0694fa4f66a1ce39940b77ffc315a621f
David Angarita <davidangarita.sistemas@gmail.com>
2025-10-09 16:23:25 -0500

    Implement DockerBackend in test_container

--- src/container_tester/__init__.py
+++ src/container_tester/__init__.py
@@ -2,15 +2,7 @@
 """Container Tester Module."""
 
 from container_tester.app import (
-    docker_client,
-    run_config,
-    run_container,
     test_container,
 )
 
-__all__ = [
-    "docker_client",
-    "run_config",
-    "run_container",
-    "test_container",
-]
+__all__ = ["test_container"]
